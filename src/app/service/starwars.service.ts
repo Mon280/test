81f359762cea493a1872d58848b6352872d2e5d1
mon280 <montserrat.mr28@gmail.com>
2024-02-16 23:31:07 -0600

    configuracion de servicio

--- src/app/service/starwars.service.ts
+++ src/app/service/starwars.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StarwarsService {
+  private api_url = 'https://swapi.dev/api/people';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getInfo(): Observable<any>{
+    return this.http.get<any>(this.api_url);
+  }
 }
